e2de7d4606ae5fd2513756086a34d341120d8889
malkiyahitesh <hiteshmalkiya8@gmail.com>
2026-05-12 15:27:53 +0530

    add sanity

--- src/lib/sanityFetch.js
+++ src/lib/sanityFetch.js
@@ -2,6 +2,6 @@ import { client } from "./sanity";
 
 export async function sanityFetch(query, params = {}) {
   return await client.fetch(query, params, {
-    next: { revalidate: 60}, // ISR caching
+    next: { revalidate: 12 * 60 * 60  }, // ISR caching
   });
 }
